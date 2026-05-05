bfa0e8900a242792bbf941552543ca87d9f87f05
ptrarvad <github@peterarvad.com>
2026-05-05 20:25:30 +0200

    Theme update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,22 @@
 export default {
-  title: "Habity Bedsideclock",
+  title: "Habity Docs",
   base: '/habity-bedsideclock-docs/',
+  appearance: 'auto',
   head: [
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
-    ['link', { href: 'https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap', rel: 'stylesheet' }]
+    ['link', { href: 'https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&family=Montserrat:wght@400;500;600&display=swap', rel: 'stylesheet' }]
   ],
   themeConfig: {
+    logo: {
+      light: 'https://habity.design/cdn/shop/files/habity_logo_black.png?v=1760530239',
+      dark: 'https://habity.design/cdn/shop/files/habity_logo_white_edb2e4c5-9e72-4486-984c-17f819ff668c.png?v=1760530405'
+    },
+    siteTitle: false,
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Getting Started', link: '/Getting started/Welcome' }
+      { text: 'Getting Started', link: '/Getting started/Welcome' },
+      { text: 'habity.design', link: 'https://habity.design', target: '_blank' }
     ],
     sidebar: [
       {
@@ -24,6 +31,10 @@ export default {
           { text: 'Troubleshooting', link: '/Troubleshooting/Troubleshooting' }
         ]
       }
-    ]
+    ],
+    footer: {
+      message: 'Habity Bedsideclock Documentation',
+      copyright: '© 2026 Habity ApS'
+    }
   }
 }
